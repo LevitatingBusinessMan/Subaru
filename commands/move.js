1b2fe6bee67e7a43b990455c31211f41705544bb
Gamerein <reinrijn@gmail.com>
2018-03-13 23:39:33 +0100

    join > move

--- commands/move.js
+++ commands/move.js
@@ -1,13 +1,12 @@
 module.exports = {
-	name: 'join',
+	name: 'move',
 	catagory: 'Music',
-	description: 'Join voice channel',
-	usage: 'join',
+	description: 'Make the bot join/move voive channels',
+	usage: 'move',
 	require: ["youtubeSearchApi"],	
 
 	run : async (Subaru, client, args, message) => {
 		try {
-			if (message.guild.voiceConnection) {Subaru.respond(message, 'I am already in a voice channel'); return;}
 			if (!message.member.voiceChannel) {Subaru.respond(message, 'You are not in a voice channel.'); return;}
 			message.member.voiceChannel.join();
 		} catch (err) {
