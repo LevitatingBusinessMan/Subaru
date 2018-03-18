928931db9882f1e6114bcc398673bbe7921c377e
Gamerein <reinrijn@gmail.com>
2018-03-18 23:42:09 +0100

    Seperated neko and neko lewd

--- commands/neko_lewd.js
+++ commands/neko_lewd.js
@@ -1,20 +1,21 @@
 module.exports = {
-	name: 'neko',
+	name: 'neko_lewd',
 	catagory: 'Image',
-	description: 'Get a neko',
-	usage: 'neko',
+	description: 'Like the neko command, but lewd. Requires the channel to be NSFW',
+	usage: 'neko_lewd',
 	
 	run : async (Subaru, client, args, message) => {
 		try {
 			
 			const request = require("axios");
-			request.get('https://nekos.life/api/v2/img/neko').then(x => Subaru.respond(message, {embed:{
+			if (message.channel.nsfw) request.get('https://nekos.life/api/v2/img/lewd').then(x => Subaru.respond(message, {embed:{
 				image: x.data,
 				color: 0x6600cc,
 				footer:{
 					text: 'powered by https://nekos.life'
 				}
 			}}));
+			else Subaru.respond(message, 'This channel isn\'t NSFW!');
 			
 		} catch (err) {
 			message.channel.send('An error occured :v');
