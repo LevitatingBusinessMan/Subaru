0718a6069f8a7028d4fd2f92e997e53d04166598
Gamerein <reinrijn@gmail.com>
2018-03-21 16:31:19 +0100

    eval now uses markdown

--- commands/eval.js
+++ commands/eval.js
@@ -8,7 +8,7 @@ module.exports = {
 	run : async (Subaru, client, args, message) => {
 		try {
 			if (!args[0]) {Subaru.respond("No argmunets!"); return}
-			Subaru.respond(message, `**input:**\n \`\`\`${args.join(" ")}\`\`\` \n **output:** \n \`\`\`${eval(args.join(" "))}\`\`\``);
+			Subaru.respond(message, `**input:**\n \`\`\`JS\n ${args.join(" ")}\`\`\` \n **output:** \n \`\`\`JS\n ${eval(args.join(" "))}\`\`\``);
 			
 		} catch (err) {
 			message.channel.send({embed: {color: 0xff0000, title: err.toString(), description: err.stack.toString()}});
