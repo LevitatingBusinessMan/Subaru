51358362d2edc6b9d78706fb29ddafe50e0be9ae
Gamerein <reinrijn@gmail.com>
2018-03-22 16:54:43 +0100

    fix npm

--- commands/npm.js
+++ commands/npm.js
@@ -6,31 +6,37 @@ module.exports = {
 	
 	run : async (Subaru, client, args, message) => {
 		try {
-			const npmSearch = require('npm-module-search');
-			if (!args[0]){ Subaru.respond(message,"Specify a module kthnx"); return; }
-			let msg = await message.channel.send("Retrieving data...");
-			npmSearch.search(args.join(" "), {limit: 1}, async (err, module) => {
-				if (err) {Subaru.log('warn', 'at: ' + message.content + '\n' + err); Subaru.respond(message,"Something went wrong :v"); return; }
-				if (!module[0]) { Subaru.respond(message,"No results"); return; }
-				msg.edit('', {embed:{
-					url: `https://www.npmjs.com/package/${module[0].name}`,
-					title: module[0].name,
+			let request = require('axios');
+			if (!args[0]){ Subaru.respond(message,"Specify a module kthnx"); return; }	
+			
+			request.get('https://www.npmjs.com/search/suggestions?q=' + args[0]).then(results => {
+				
+				let module = results.data[0];
+				
+				Subaru.respond(message, {embed:{
+					url: module.links.npm,
+					title: module.name,
 					color: 0xff0000,
 					fields: [{
 						name: "Description:",
-						value: module[0].description
+						value: module.description
 					},{
 						name: "Version:",
-						value: module[0].version
+						value: module.version
 					},{
 						name: "Author:",
-						value: module[0].author
+						value: module.author.name
 					}],
 					footer: {
-						text: module[0].name + ' | ' + module[0].version + ' | ' + module[0].author
+						text: module.keywords.join(' | ')
 					}
 				}});
-			});
+				
+				}).catch(err => {
+					Subaru.respond(message, 'Something went wrong :v');
+					Subaru.log('warn', err.toString());
+				});
+				
 		} catch (err) {
 			message.channel.send('An error occured :v');
 			Subaru.error(err, message);
