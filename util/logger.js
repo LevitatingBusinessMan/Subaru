6399abfbac545b849932cddde54d31381a32a212
Gamerein <reinrijn@gmail.com>
2018-04-02 22:27:19 +0200

    Logger now has 0's fixed

--- util/logger.js
+++ util/logger.js
@@ -1,8 +1,13 @@
 module.exports = (rType, msg) => {
 	const chalk = require('chalk');
 	let d = new Date();
+	
+	let hour = d.getHours().toString() + (d.getHours().toString().length == 1 ? '0' : "");
+	let minutes = d.getMinutes().toString() + (d.getMinutes().toString().length == 1 ? '0' : "");
+	let seconds = d.getSeconds().toString() + (d.getSeconds.toString().length == 1 ? '0' : "");
+		
 	let type = rType.toUpperCase();
-	if (type == 'OK') console.log(`[${chalk.green(type + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds())}] ${msg}`);
-	if (type == 'WARN')  console.log(`[${chalk.yellow(type + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds())}] ${msg}`);
-	if (type == 'ERR') console.log(`[${chalk.red(type + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds())}] ${msg}`);	
+	if (type == 'OK') console.log(`[${chalk.green(type + ' ' + hour + ':' + minutes + ':' + seconds)}] ${msg}`);
+	if (type == 'WARN')  console.log(`[${chalk.yellow(type + ' ' + hour + ':' + minutes + ':' + seconds)}] ${msg}`);
+	if (type == 'ERR') console.log(`[${chalk.red(type + ' ' + hour + ':' + minutes + ':' + seconds)}] ${msg}`);	
 }
